ca448a4932fdb3c935e39a34f5fdc19e508fa2a3
Antony CASTANER <antony.castanerpynov.com>
2019-11-24 19:02:19 +0100

    Ask question and answer

--- javascript/Game.js
+++ javascript/Game.js
@@ -7,12 +7,23 @@ class Game {
      */
     constructor() {
         this.main = document.querySelector("main");
-        this.data = [
-            "Bonjour, je m'appelle Zion, je serai votre guide tout au long de votre périple. Quel est votre prénom ?",
-            "C'est original : je ne connaissais pas ce prénom ! Vous devez sûrement venir du passée. Non ?"
+        this.reply = [
+            "Bonjour, je m'appelle Zion, je serai votre guide tout au long de votre périple. Quel est votre prénom ?"
         ];
-        this.answers = [];
-        this.reply = 0;
+        this.replyCount = 0;
+        this.answers = [
+            [
+                "C'est original : je ne connaissais pas ce prénom ! Vous devez sûrement venir du passée. Non ?",
+                "Vous avez du faire un long voyage alors.",
+                "Vos parents sont nostalgiques !"
+            ],
+            [
+                "Vous avez été invité à me rejoindre dans ce lieu atypique, nous sommes au sommet de la plus grande tour du monde terrien. Connaissez-vous la raison de cet invitation ?",
+                "Parfait ! Je vais tout de même vous rappeler l'importance de votre venue.",
+                "Je vais alors vous expliquer votre venue."
+            ]
+        ];
+        this.answersCount = 0;
         this.showStartButton();
     }
 
@@ -41,20 +52,80 @@ class Game {
     /**
      * Create and add p tag with reply from narrator to main tag
      */
-    showReply() {
+    showReply(value = this.reply[this.replyCount]) {
         let text = document.createElement("p");
-        text.innerHTML = this.data[this.reply];
-        this.reply++;
+        text.innerHTML = value;
+        if (value === this.reply[this.replyCount]) {
+            this.replyCount++;
+        }
         this.main.appendChild(text);
     }
 
+    /**
+     * Show the question
+     */
+    showQuestion() {
+        let question = document.createElement("p");
+        let firstAnwser = document.createElement("button");
+        let secondAnswer = document.createElement("button");
+
+        question.innerHTML = this.answers[this.answersCount][0];
+        firstAnwser.innerHTML = "Oui";
+        firstAnwser.addEventListener("click", function () {
+            game.showAnswer(true);
+        });
+        secondAnswer.innerHTML = "Non";
+        secondAnswer.addEventListener("click", function () {
+            game.showAnswer(false);
+        });
+
+        this.main.appendChild(question);
+        this.main.appendChild(firstAnwser);
+        this.main.appendChild(secondAnswer);
+    }
+
+    /**
+     * Show one of the answer
+     *
+     * @param check
+     *              Yes or no / True or false
+     */
+    showAnswer(check) {
+        let answer = document.createElement("p");
+        answer.innerHTML = this.answers[this.answersCount][(check) ? 1 : 2];
+        this.main.appendChild(answer);
+
+        let buttons = document.getElementsByTagName("button");
+        buttons[1].remove();
+        buttons[0].remove();
+    }
+
+    /**
+     * Create and add form to enter hero name
+     */
     showForm() {
         let form = document.createElement("form");
         let input = document.createElement("input");
+        // Add input to form
         form.appendChild(input);
         form.addEventListener("submit", function (evt) {
             evt.preventDefault();
-        })
+            // Create hero
+            hero = new Hero(input.value);
+            // Remove form on submit
+            form.remove();
+            // Show name of hero
+            game.showReply(input.value);
+            game.ask();
+        });
         this.main.appendChild(form);
+        document.getElementsByTagName("input")[0].focus();
+    }
+
+    /**
+     *
+     */
+    ask() {
+        game.showQuestion();
     }
 }
